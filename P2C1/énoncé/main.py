0993577df97548d20845b1933dfd134a2e9ef73b
Ranga Gonnage <rangagonnage@MacBook-Pro-de-Ranga.local>
2024-03-26 16:36:37 +0100

    P2C1

--- P2C1/énoncé/main.py
+++ P2C1/énoncé/main.py
@@ -1,6 +1,6 @@
 def main():
-	# Ecrivez votre code ici !
-	# Attention tout votre code doit être indenté comme ce commentaire
+    # Ecrivez votre code ici !
+    # Attention tout votre code doit être indenté comme ce commentaire
 
 
 # Ne touchez pas le code ci-dessous

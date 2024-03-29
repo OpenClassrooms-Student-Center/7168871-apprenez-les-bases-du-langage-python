2cf14b54f2ab58c32d3154c95c6af9f32452a0d7
Ranga Gonnage <rangagonnage@MacBook-Pro-de-Ranga.local>
2024-03-29 11:47:34 +0100

    P3C1

--- P3/P3C1/correction/main.py
+++ P3/P3C1/correction/main.py
@@ -7,7 +7,7 @@ def main():
     # Utilisation de la fonction addition
     resultat_addition = operations.addition(3, 5)
     print("Résultat de l'addition :", resultat_addition)
-    # Utilisation de la fonction soustraction
+    # Utilisation de la fonction multiplication
     resultat_multiplication = operations.multiplication(8, 2)
     print("Résultat de la multiplication :", resultat_multiplication)
 

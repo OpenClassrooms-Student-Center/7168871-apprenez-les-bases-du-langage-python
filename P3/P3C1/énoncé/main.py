284cdc70cf1501448158f9a3776d606505d35bb2
Ranga Gonnage <rangagonnage@MacBook-Pro-de-Ranga.local>
2024-03-29 11:35:50 +0100

    P3C1

--- P3/P3C1/énoncé/main.py
+++ P3/P3C1/énoncé/main.py
@@ -1,4 +1,7 @@
-# Écrivez votre code ici !
+def main():
+    # Ecrivez votre code ici !
+    # Attention tout votre code doit être indenté comme ce commentaire
+
 
 # Ne touchez pas le code ci-dessous
 if __name__ == "__main__":

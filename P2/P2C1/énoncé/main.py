6bf4539f7c3419e895dd084fd7b579fadf653a6c
SalahMaroc <166803533+SalahMaroc@users.noreply.github.com>
2024-04-13 15:26:09 +0000

    Update main.py

--- P2/P2C1/énoncé/main.py
+++ P2/P2C1/énoncé/main.py
@@ -1,8 +1,37 @@
 def main():
-    # Ecrivez votre code ici !
-    # Attention tout votre code doit être indenté comme ce commentaire
+    nombre_a_gauche = input("Entrez un nombre entier : ")
+    nombre_a_droite = input("Entrez un nombre entier : ")
+    operation = input("Entrez l'opération souhaitée ['+', '-', '*' ou '/'] : ")
 
+    resultat = 0
 
-# Ne touchez pas le code ci-dessous
+    # Pour vérifie si les deux nombres sont valides avec la fonction
+    # soit un integer, ou un float
+    if not nombre_a_gauche.isnumeric() or not nombre_a_droite.isnumeric():
+        print("Erreur: les deux nombres doivent être des nombres entiers")
+    else:
+        nombre_a_gauche = int(nombre_a_gauche)
+        nombre_a_droite = int(nombre_a_droite)
+        
+        match operation:
+            case "+":
+                resultat = nombre_a_gauche + nombre_a_droite
+            case "-":
+                resultat = nombre_a_gauche - nombre_a_droite
+            case "*":
+                resultat = nombre_a_gauche * nombre_a_droite
+            case "/":
+                # Pour Vérifier si variable `nombre_a_droite` n'est pas nulle pour la division
+                if nombre_a_droite == 0:
+                    print("Erreur: impossible de diviser par zéro.")
+                else:
+                    resultat = nombre_a_gauche / nombre_a_droite
+            # Si le symbole est incorrect.
+            case _:
+                print("Erreur: le symbole d'opération doit être '+', '-', '*' ou '/'.")
+
+        # Affiche le résultat
+        print(f"Le résultat de l'opération est: {resultat}")
+
 if __name__ == "__main__":
     main()

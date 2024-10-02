8c30f7ee1a114e6a2240967775700be8cd2b3ff0
warrior981 <57837288+warrior981@users.noreply.github.com>
2024-10-02 22:12:22 +0200

    Update main.py

--- P1/P1C5/énoncé/main.py
+++ P1/P1C5/énoncé/main.py
@@ -1,1 +1,13 @@
 # Écrivez votre code ici !
+nom = "maxime"
+age = 14
+taille = 165.5
+est-etudiant = false
+print(nom)
+print (age)
+print (taille)
+print (est-etudiant)
+type (age)
+type (nom)
+type (taile)
+type (est-etudiant)

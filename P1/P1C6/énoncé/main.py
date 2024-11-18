8ee4e40d36144435b2d47df960a703c5756678ec
Enishu <pe.labigne@gmail.com>
2024-11-18 10:39:21 +0100

    Update main.py

--- P1/P1C6/énoncé/main.py
+++ P1/P1C6/énoncé/main.py
@@ -1,1 +1,11 @@
 # Écrivez votre code ici !
+fruits = ['pomme', 'banane' , 'orange']
+fruits.append('kiwi')
+print(fruits)
+del (fruits[2])
+print(fruits)
+fruits[1] = "ananas"
+print(fruits)
+print("La liste contient", len(fruits), "fruits.")
+fruits.sort()
+print(fruits)

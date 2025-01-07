a66c3a024e783110dbc3af14d7cd9c2b44f474b2
MaguetteN <151390375+MaguetteN@users.noreply.github.com>
2025-01-07 15:17:14 +0000

    Update main.py

--- P1/P1C5/énoncé/main.py
+++ P1/P1C5/énoncé/main.py
@@ -1,1 +1,14 @@
-# Écrivez votre code ici !
+nom= "ndiaye"
+prenom=" ndeye maguette"
+age=22
+taille=1,75
+est_etudiant=True
+print(nom)
+print(age)
+print(taille)
+print(est_etudiant)
+type(nom)
+type(age)
+type(taille)
+type(est_etudiant)
+

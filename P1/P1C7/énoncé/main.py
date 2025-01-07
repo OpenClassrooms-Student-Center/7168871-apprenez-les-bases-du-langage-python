57ad9a3f58ca317afbcbe54345d35d9616876ac3
MaguetteN <151390375+MaguetteN@users.noreply.github.com>
2025-01-07 16:54:49 +0000

    Update main.py

--- P1/P1C7/énoncé/main.py
+++ P1/P1C7/énoncé/main.py
@@ -1,1 +1,10 @@
 # Écrivez votre code ici !
+fruits={"pomme":"rouge", "banane":"jaune","orange":"orange"}
+print(fruits)
+fruits['kiwi']= "vert"
+couleur_banane=fruits['banane']
+print(couleur_banane)
+fruits['pomme']=" vert"
+print(fruits)
+del fruits['banane']
+print(fruits)

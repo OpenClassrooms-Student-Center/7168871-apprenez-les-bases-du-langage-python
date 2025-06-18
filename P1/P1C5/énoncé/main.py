a3bd7b37b91b9ba410729744be4ca9163efe6483
ekonomee <reyan3rd@gmail.com>
2025-06-18 12:54:06 +0200

    Update main.py

--- P1/P1C5/énoncé/main.py
+++ P1/P1C5/énoncé/main.py
@@ -1,1 +1,10 @@
-# Écrivez votre code ici !
+nom = nome
+age = 20
+taille = 1.85
+est_etudiant = true
+
+print(nom, age, taille, est_etudiant)
+type(nom)
+type(age)
+type(taille)
+type(est_etudiant)
